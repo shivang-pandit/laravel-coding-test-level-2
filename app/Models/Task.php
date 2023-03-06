ffc44d614b31667e7082f1bb8e25f5742e0d8780
Shivang <shivang512@gmail.com>
2023-03-06 17:31:09 +0530

    Create models

---
+++ app/Models/Task.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    public $incrementing = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'id',
+        'title',
+        'description',
+        'status',
+        'project_id',
+        'user_id',
+    ];
+}
